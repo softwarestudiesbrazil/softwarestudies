427d91385db2a9ba675b7ad28faab29104df29e0
sunsern <sunsern@f0e247ac-3f28-11de-8037-d3acef5c3621>
2009-05-13 08:34:01 +0000

    add test script; add dist/softwarestudies.jar

--- matlab/colorTexture/batch.m
+++ matlab/colorTexture/batch.m
@@ -1,50 +1,35 @@
-%-----------------------------------------
-%Routine to run a batch processing 
-%-----------------------------------------------------
+function batch(inputPath)
 
 close all; 
-%disp('***********************************')
-%disp('Culturevis - http://culturevis.com/')
-%disp('This code extract color and texture features of images from folders specified in input.txt file')
-%donehomework = input('Did you create a text file (input.txt) with folders to be investigated (y/n)?','s');
-%disp('***********************************')
-%if strcmp(donehomework,'y')
 
-myfolders = initialize();%init routine
+myfolders = cell(1,1);
+myfolders{1}{1} = inputPath;
 nfolders = length(myfolders{1});    
 for j=1:nfolders
     currfolder = myfolders{1}{j};
     myfiles=dir([currfolder,'*.jpg']); 
     nimages = length(myfiles);
-    disp(['Processing folder ',currfolder, ' with ', num2str(nimages), ' images....']);
+    disp(['Processing folder ' currfolder ' with ' num2str(nimages) ' images...']);
 
-    % get directory name
-    C = textscan(currfolder, '%s', 'delimiter', '/');
-    
-    resultf = fopen([currfolder char(C{1}(end)) '.result.txt'],'w');
+    resultf = fopen([currfolder 'stats.txt'],'w');
     printHeader(resultf);
     for i=1:nimages
         disp(['..',myfiles(i).name]);
         Image = imread([currfolder,myfiles(i).name]);
+
         %6 features for each channel = 3*6 = 18
-        color_feat_rgb = colorAtrib(Image); % you may want to choose one of the two, they can be extremely correlated!
-        %6 features for each channel = 3*6 = 18
-	%color_feat_lab = colorAtribLab(Image);        
+        color_feat_rgb = colorAtrib(Image); 
+
         %13 features
         glcm = h_cooccurrence(rgbToGray(Image),1,256); %you may want to vary the parameters 1,256
         texture_feat=h_getTextFeat(glcm);
-        vfeat = [color_feat_rgb color_feat_lab texture_feat];
-        %featurefile = [currfolder myfiles(i).name(1:end-4) '.txt'];
-        %saveTable(vfeat,featurefile);
+
+        vfeat = [color_feat_rgb texture_feat];
         printResult(resultf, myfiles(i).name, vfeat);
     end
-fclose(resultf);
+    fclose(resultf);
 end %end nfolders
 
-%else
-%    disp('Please create a txt file with folders to be investigated')
-%    disp('Example of content of an input.txt:')
-%    disp('/u5/yourname/Culturevis/')
-%    disp('*** remember to start and finish with / ***')
-%end
 disp('Done')    
+
+end
