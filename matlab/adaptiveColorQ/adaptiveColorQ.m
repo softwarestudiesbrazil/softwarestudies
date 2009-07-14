51141e274f1fbc3a6fe5e5e76995f29d105a37c9
sunsern <sunsern@f0e247ac-3f28-11de-8037-d3acef5c3621>
2009-07-14 00:18:49 +0000

    add adaptiveColorQ and update ca_analyze.py accordingly

---
+++ matlab/adaptiveColorQ/adaptiveColorQ.m
@@ -0,0 +1,48 @@
+function adaptiveColorQ(directory,n,k,outputImg)
+
+if nargin < 2
+    n = 16;
+end
+
+if nargin < 3
+    k = 2;
+end
+
+if nargin < 4
+    outputImg = 0;
+end
+
+files = dir(fullfile(directory,'*.jpg'));
+nimages = length(files);
+
+if nimages > 0
+    resultf = fopen(fullfile(directory,'adaptiveColorQ_result.txt'),'w');
+    
+    % column label
+    fprintf(resultf,'filename');
+    for j=1:k
+        fprintf(resultf,...
+            ',Color%d_R,Color%d_G,Color%d_B,Color%d_Percentage',j,j,j,j);
+    end
+    fprintf(resultf,'\n');
+    
+    % data type
+    fprintf(resultf,'string');
+    for j=1:k
+        fprintf(resultf,',float,float,float,float');
+    end
+    fprintf(resultf,'\n');
+    
+    for i=1:nimages
+        full_filename = fullfile(directory,files(i).name);
+        disp(['[matlab-adaptiveColorQ]..' full_filename]);
+        T = color_rgb(full_filename,n,k,outputImg);
+        fprintf(resultf,'%s',files(i).name);
+        for j=1:k
+            fprintf(resultf,',%.1f,%.1f,%.1f,%.4f',T(j,:));
+        end
+        fprintf(resultf,'\n');
+    end
+    fclose(resultf);
+end
+end
