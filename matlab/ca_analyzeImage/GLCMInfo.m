9be4a27aa88647431f91d73237f508783c596c52
jeremydouglass <jeremydouglass@f0e247ac-3f28-11de-8037-d3acef5c3621>
2009-09-03 21:24:45 +0000

    added ca_analyzeImage -- simple matlab for NERSC HHPC

---
+++ matlab/ca_analyzeImage/GLCMInfo.m
@@ -0,0 +1,39 @@
+
+function [output,header] = GLCMInfo(I,D)
+
+% GLCMInfo extract GLCM information
+% I = RGB or grayscale image
+% D = distance for GLCM
+% output = [contrast, 
+%           correlation,
+%           energy, 
+%           homogeneity]
+
+  % by default, output values are zeros
+  output = zeros(1,4);
+  header = cell(1,4);
+ 
+  % if I is rgb, convert to grayscale
+  if (ndims(I) == 3 && size(I,3) == 3)
+    I = rgb2gray(I);
+  end 
+  
+  % compute GLCM and its stats
+  offsets = [0 D; -D D; -D 0; -D -D];
+  GLCMs = graycomatrix(I,'Offset',offsets);
+  stats = graycoprops(GLCMs,{'Contrast',...
+                             'Correlation',...
+                             'Energy',...
+                             'Homogeneity'});
+  
+  output(1,1) = mean(stats.Contrast);
+  header(1,1) = cellstr(['Contrast_' num2str(D)]);
+  output(1,2) = mean(stats.Correlation);
+  header(1,2) = cellstr(['Correlation_' num2str(D)]);
+  output(1,3) = mean(stats.Energy);
+  header(1,3) = cellstr(['Energy_' num2str(D)]);
+  output(1,4) = mean(stats.Homogeneity);
+  header(1,4) = cellstr(['Homogeneity_' num2str(D)]);
+
+  
+end
