b223a3fab80fa177eb05848bb25dbe37c89c9f52
sunsern <sunsern@f0e247ac-3f28-11de-8037-d3acef5c3621>
2009-10-29 23:59:48 +0000

    enable IPT in matlab scripts

--- matlab/ca_analyzeImage/basicInfo.m
+++ matlab/ca_analyzeImage/basicInfo.m
@@ -1,36 +1,44 @@
 
-function [output,header] = basicInfo(I)
+function [output,header] = basicInfo(I,IPT)
 
 % basicInfo extract basic information
 % I = RGB or grayscale image
-% output = [entropy, 
-%           mean, 
+% IPT = true if IPT installed
+% output = [entropy,
+%           mean,
 %           std,
 %           edgeAmountSobel]
 
-  % by default, output values are zeros
-  output = zeros(1,2);
-  header = cell(1,2);
-  
-  % if I is rgb, convert to grayscale
-  if (ndims(I) == 3 && size(I,3) == 3)
-    %I = rgb2gray(I);
-    I = 0.2989*I(:,:,1) + 0.5870*I(:,:,2) + 0.1140*I(:,:,3);
-  end 
- 
-%   output(1,1) = entropy(I);
-%   header(1,1) = cellstr('Entropy');
-  
-  % convert to double for mean and std calculation
-  Id = double(I(:));
-  output(1,1) = mean(Id);
-  header(1,1) = cellstr('Mean');
-  output(1,2) = std(Id);
-  header(1,2) = cellstr('Std');
-  
-  % Sobel edge ratio 
-%   edgeAmountSobel = sum(sum(edge(I,'sobel')))/(size(I,1)*size(I,2));
-%   output(1,4) = edgeAmountSobel;
-%   header(1,4) = cellstr('Sobel');
-%   
+% by default, output values are zeros
+output = [];
+header = {};
+
+% if I is rgb, convert to grayscale
+if (ndims(I) == 3 && size(I,3) == 3)
+    if (IPT)
+        I = rgb2gray(I);
+    else
+        I = 0.2989*I(:,:,1) + 0.5870*I(:,:,2) + 0.1140*I(:,:,3);
+    end
+end
+
+% convert to double for mean and std calculation
+Id = double(I(:));
+output(1,1) = mean(Id);
+header(1,1) = cellstr('Mean');
+output(1,2) = std(Id);
+header(1,2) = cellstr('Std');
+
+if (IPT)
+    output(1,3) = entropy(I);
+    header(1,3) = cellstr('Entropy');
+end
+
+% Sobel edge ratio
+if (IPT)
+    edgeAmountSobel = sum(sum(edge(I,'sobel')))/(size(I,1)*size(I,2));
+    output(1,4) = edgeAmountSobel;
+    header(1,4) = cellstr('Sobel');
+end
+
 end
