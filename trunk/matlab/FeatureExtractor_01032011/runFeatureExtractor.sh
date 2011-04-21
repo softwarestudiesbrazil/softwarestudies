1f8b2ff29138b0b5ec57103ed538572737701e2e
sunsern@gmail.com <sunsern@gmail.com@f0e247ac-3f28-11de-8037-d3acef5c3621>
2011-04-21 18:28:04 +0000

    Add wrapper scripts for FeatureExtractor

---
+++ trunk/matlab/FeatureExtractor_01032011/runFeatureExtractor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: `basename $0` {image path} {prefix}"
+    exit
+fi
+
+SCRIPTPATH=$0;
+SCRIPTPATH=${SCRIPTPATH%/*}
+matlab -nodisplay -r "path(path,'$SCRIPTPATH');FeatureExtractor('$1','$2');exit;"
