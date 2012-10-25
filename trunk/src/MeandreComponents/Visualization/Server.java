948a2160088667377f09310269e9ce3b94a2d34b
ommirbod@eng.ucsd.edu <ommirbod@eng.ucsd.edu@f0e247ac-3f28-11de-8037-d3acef5c3621>
2012-10-25 16:41:43 +0000

    User can upload a directory, server will download all images in that directory, run FeatureExtractor, and place resulting text files on the server

--- trunk/src/MeandreComponents/Visualization/Server.java
+++ trunk/src/MeandreComponents/Visualization/Server.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.net.*;
 
+import org.apache.commons.io.FileUtils;
+
 public class Server{
 	ServerSocket providerSocket;
 	Socket connection = null;
@@ -28,30 +30,31 @@ public class Server{
 			in = new ObjectInputStream(connection.getInputStream());
 			sendMessage("Connection successful");
 			//4. The two parts communicate via the input and output streams
+			int count=0;
 			do{
 				try{
 					//////
 					file = (Object[])in.readObject();
-					String filename = (String) file[0];
-					System.err.println("name is: "+filename);
-					byte[] filebytes = (byte[]) file[1];
-					FileOutputStream fileout = new FileOutputStream(filename);
-					fileout.write(filebytes);
-					//File f = new File(filename);
-					//FileUtils.writeByteArrayToFile(f, filebytes);
-					sendMessage("end");
-					System.exit(0);
-					/////
+					writeFile(file,"/Users/culturevis/Documents/MeandreTesting/ImageAnalyze/ClientImages/");
+					sendMessage("Server downloaded file: "+(String) file[0]);
 					
-					message = (String)in.readObject();
-					System.out.println("client>" + message);
+					/////
+					count+=1;
+					//message = (String)in.readObject();
+					//System.out.println("client>" + message);
+					if(count==4){
+						writeConfigFile("/Users/culturevis/Documents/MeandreTesting/ImageAnalyze/ClientImages/");
+						runFeatureExtractor("/Users/culturevis/Documents/MeandreTesting/ImageAnalyze/ClientImages/");
+						System.exit(0);
+					}else 
+						continue;
 					if (message.equals("end"))
 						sendMessage("end");
 				}
 				catch(ClassNotFoundException classnot){
 					System.err.println("Data received in unknown format");
 				}
-			}while(!message.equals("end"));
+			}while(true);
 		}
 		catch(IOException ioException){
 			ioException.printStackTrace();
@@ -79,6 +82,64 @@ public class Server{
 			ioException.printStackTrace();
 		}
 	}
+	
+	void writeFile(Object[] file, String path)
+	{
+		try{
+			String filename = (String) file[0];
+			System.err.println("name is: "+filename);
+			byte[] filebytes = (byte[]) file[1];
+			FileOutputStream fileout = new FileOutputStream(path+filename);
+			fileout.write(filebytes);
+			File f = new File(path+filename);
+			FileUtils.writeByteArrayToFile(f, filebytes);
+		} catch(Exception e){e.printStackTrace();}
+	}
+	
+	void writeConfigFile(String path)
+	{
+		Writer output = null;
+		File configFile = new File(path+"paths.txt");
+		try {
+		output = new BufferedWriter(new FileWriter(configFile));
+			String files;
+			File folder = new File(path);
+			File[] listOfFiles = folder.listFiles();
+			for (int i = 0; i < listOfFiles.length; i++) 
+			{
+				if (listOfFiles[i].isFile()) 
+				{
+					files = listOfFiles[i].getPath();
+					if(listOfFiles[i].getName().equals("paths.txt")) continue;
+					output.write(files+"\n");
+				}
+			}
+			output.close();
+			File scriptFile = new File(path+"runFeatureExtractor.sh");
+			output = new BufferedWriter(new FileWriter(scriptFile));
+			output.write("matlab -nodisplay -r \"path(path,'/Applications/Programming/softwarestudies/matlab/FeatureExtractor'); FeatureExtractor('"+path+"paths.txt', '"+path+"results'); exit;\"");
+			output.close();
+		} catch (IOException e) {e.printStackTrace();}
+	}
+	
+	void runFeatureExtractor(String path)
+	{
+		Runtime rt = Runtime.getRuntime();
+		Process p = null;
+		String permissionCommand = "chmod +x "+path+"runFeatureExtractor.sh";
+		String runCommand = path+"runFeatureExtractor.sh";
+		String removeCommand = "rm "+path+"runFeatureExtractor.sh";
+		try{
+			p = rt.exec(permissionCommand);
+			p.waitFor();
+			p = rt.exec(runCommand);
+			p.waitFor();
+			p = rt.exec(removeCommand);
+			p.waitFor();
+		} catch(Exception e){e.printStackTrace();}
+	    InputStream output = p.getInputStream();
+	    //System.out.println( output );
+	}
 	public static void main(String args[])
 	{
 		Server server = new Server();
@@ -87,3 +148,7 @@ public class Server{
 		}
 	}
 }
+
+/*
+matlab -nodisplay -r "path(path,'/Applications/Programming/softwarestudies/matlab/FeatureExtractor'); FeatureExtractor('paths.txt', 'results'); exit;"
+*/
