fe16f4d8f2187d0cbbaa56c32fb9b40b36e68bd9
ommirbod@eng.ucsd.edu <ommirbod@eng.ucsd.edu@f0e247ac-3f28-11de-8037-d3acef5c3621>
2012-11-08 14:36:52 +0000

    Added logging system of jobs from PID.java to UnixCommands.java (since main project calls UnixCommands.java)

--- src/MeandreComponents/Visualization/PID.java
+++ src/MeandreComponents/Visualization/PID.java
@@ -1,13 +1,27 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+import au.com.bytecode.opencsv.CSVReader;
 
 
 public class PID {
 	
 	public static void main(String[] args){
+		int id=0;
+		try{
+			CSVReader reader = new CSVReader(new FileReader("PIDlog.csv"));
+		    String [] nextLine;
+		    int prev=0;
+		    while ((nextLine = reader.readNext()) != null) {
+		    	prev=Integer.parseInt(nextLine[1]);
+		    }
+		    id = (prev != 0) ? (prev+1) : 1; 
+		    
+		} catch (Exception e){e.printStackTrace();}
 		String imgFilePaths = "/Users/culturevis/Documents/MeandreTesting/ImageAnalyze/images";
-		String[] runCommand = new String[] {"sh", "-c","matlab -nodisplay -r \"path(path,'/Applications/Programming/softwarestudies/matlab/FeatureExtractor'); FeatureExtractor('"+imgFilePaths+"', '"+imgFilePaths+"/results'); exit;\" & PID=$!; echo $PID,0,started >> PIDlog.csv"};
+		String[] runCommand = new String[] {"sh", "-c","matlab -nodisplay -r \"path(path,'/Applications/Programming/softwarestudies/matlab/FeatureExtractor'); FeatureExtractor('"+imgFilePaths+"', '"+imgFilePaths+"/results'); exit;\" & PID=$!; echo $PID,"+id+",started,$(date +'%F %T') >> PIDlog.csv"};
 		
 		String line;
 		//execute command
@@ -20,13 +34,10 @@ public class PID {
 			BufferedReader in = new BufferedReader(
 		               new InputStreamReader(p.getInputStream()) ); //interesting...must open input stream on java end or else matlab can't export to files
 		       while ((line = in.readLine()) != null) {
-		         //System.out.println(line); //don't output to screen, just write the files
+		         //System.out.println(line); //don't output to screen, just write the files 
 		       }
 		       in.close();
 			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} catch (Exception e) {e.printStackTrace();}
 	}
 }
--- src/MeandreComponents/Visualization/UnixCommands.java
+++ src/MeandreComponents/Visualization/UnixCommands.java
@@ -1,8 +1,13 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Writer;
+
+import au.com.bytecode.opencsv.CSVReader;
 
 /**
  * 
@@ -31,6 +36,42 @@ public class UnixCommands {
 	 * TODO need a method to keep track of progress, listening to generated log file and
 	 * 		comparing it to list of imgFilePaths could be one method.
 	 */
+	
+	public void RunFeatureExtractor(String imgFilePaths){
+		int id=0;
+		try{
+			CSVReader reader = new CSVReader(new FileReader("PIDlog.csv"));
+		    String [] nextLine;
+		    int prev=0;
+		    while ((nextLine = reader.readNext()) != null) {
+		    	prev=Integer.parseInt(nextLine[1]);
+		    }
+		    id = (prev != 0) ? (prev+1) : 1; 
+		    
+		} catch (Exception e){e.printStackTrace();}
+		//String imgFilePaths = "/Users/culturevis/Documents/MeandreTesting/ImageAnalyze/images";
+		String[] runCommand = new String[] {"sh", "-c","matlab -nodisplay -r \"path(path,'/Applications/Programming/softwarestudies/matlab/FeatureExtractor'); FeatureExtractor('"+imgFilePaths+"', '"+imgFilePaths+"results'); exit;\" & PID=$!; echo $PID,"+id+",started,$(date +'%F %T') >> PIDlog.csv"};
+		
+		String line;
+		//execute command
+		Runtime rt = Runtime.getRuntime();
+		Process p = null;
+		try {
+			p = rt.exec(runCommand);
+			p.waitFor();
+			
+			BufferedReader in = new BufferedReader(
+		               new InputStreamReader(p.getInputStream()) ); //interesting...must open input stream on java end or else matlab can't export to files
+		       while ((line = in.readLine()) != null) {
+		         //System.out.println(line); //don't output to screen, just write the files 
+		       }
+		       in.close();
+			
+		} catch (Exception e) {e.printStackTrace();}
+		this.feedbackMessage = "FeatureExtractor Executed with code: "+p.exitValue();
+	}
+
+	/*
 	public void RunFeatureExtractor(String imgFilePaths,String imgFilePathsDir){
 		Writer output = null;
 		File scriptFile = new File(imgFilePathsDir+"/runFeatureExtractor.sh");
@@ -59,7 +100,7 @@ public class UnixCommands {
 		}
 		this.feedbackMessage = "FeatureExtractor Executed with code: "+p.exitValue();
 	}
-	
+	*/
 	public String getMessage(){
 		return this.feedbackMessage;
 	}
