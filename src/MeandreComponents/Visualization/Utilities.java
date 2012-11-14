a0d4acefb36ab4e9d4026bb8d253c2a8ca9407d9
ommirbod@eng.ucsd.edu <ommirbod@eng.ucsd.edu@f0e247ac-3f28-11de-8037-d3acef5c3621>
2012-11-14 17:01:13 +0000

    added output mechanisms for result files either by byte[] array or Vector object log file gets updated now when tasks are finished, log includes path user provided

--- src/MeandreComponents/Visualization/Utilities.java
+++ src/MeandreComponents/Visualization/Utilities.java
@@ -3,21 +3,27 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.util.Vector;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+/*
 import org.apache.commons.compress.archivers.ArchiveOutputStream;
 import org.apache.commons.compress.archivers.ArchiveStreamFactory;
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.utils.IOUtils;
+*/
+import au.com.bytecode.opencsv.CSVReader;
 
 public class Utilities {
 	
 	static byte[] buffer = new byte[1024];
 	//public static String zipfile(String[] files){
-
+/*
 	public static void zip(String[] args){
 		try{
 			final OutputStream out = new FileOutputStream("C:\\Users\\ommirbod\\Desktop\\javazipped.zip");
@@ -37,7 +43,8 @@ public class Utilities {
 	        os.close(); 
 		} catch(Exception e){e.printStackTrace();}
     }
-	
+*/
+/*	
 	public static void sendFile(OutputStream out, File file){
 		byte[] bytearray = new byte[(int) file.length()];
 		try{
@@ -47,5 +54,18 @@ public class Utilities {
 		    out.flush();
 		}
 		catch(Exception e){e.printStackTrace();}
+	}
+*/	
+	public static void sendFileVector(ObjectOutputStream out, File file){
+		Vector v = new Vector();
+		try {
+			CSVReader reader = new CSVReader(new FileReader(file.getAbsolutePath()), '\t');
+			String [] nextLine;
+		    while ((nextLine = reader.readNext()) != null)
+		        v.add(nextLine);
+		    
+		    out.writeObject(v);
+		    System.err.println("vector file sent");
+		} catch (Exception e) { e.printStackTrace(); }
 	}
 }
