de6b40dbfcc6bda2623f3370a8de81c12919d18b
ommirbod@eng.ucsd.edu <ommirbod@eng.ucsd.edu@f0e247ac-3f28-11de-8037-d3acef5c3621>
2012-12-18 17:55:57 +0000

    * Tested java sorting library on ImageAnalyze result files, works on multiple columns and recognizes NaN. Need to rewrite files to exclude header information before sorting. Still need to write file on server side

--- trunk/src/MeandreComponents/Visualization/ImageVisualizeComponent.java
+++ trunk/src/MeandreComponents/Visualization/ImageVisualizeComponent.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Writer;
+
 import org.meandre.annotations.Component;
 import org.meandre.annotations.ComponentInput;
 import org.meandre.annotations.ComponentOutput;
@@ -79,6 +85,28 @@ public class ImageVisualizeComponent extends AbstractExecutableComponent {
 		Object input_2 = cc.getDataComponentFromInput(IN_COMMAND);
 		String InputFilePath[] = DataTypeParser.parseAsString(input);
 		String InputCommand[] = DataTypeParser.parseAsString(input_2);
+		///
+		//see if sorting needs to be done first
+		int sortIndex = InputCommand[0].lastIndexOf("sort");
+		//only sort if user has entered sort command
+		if(sortIndex != -1){
+			String sortCommand = InputCommand[0].substring(InputCommand[0].lastIndexOf("sort")+5,InputCommand[0].lastIndexOf("/"));
+			//depends if I am taking file from client end or file residing on server end
+			String file = InputCommand[0].substring(InputCommand[0].lastIndexOf("/"),InputCommand[0].length());
+			//rewrite file without headers
+			Writer output = new BufferedWriter(new FileWriter(file+"tmp"));
+			BufferedReader readbuffer = new BufferedReader(new FileReader(file));
+			
+			//sort file
+			Runtime rt = Runtime.getRuntime();
+			Process p = null;
+			String[] runCommand = new String[] {"sh","-c",file+" "+sortCommand};
+		    p = rt.exec(runCommand);
+		    p.waitFor();
+			//sorted file has same file name
+	    
+		} 
+		
 		ClientImageVisualize client = new ClientImageVisualize(InputFilePath[0],InputCommand[0]);
 		client.run();
 		cc.pushDataComponentToOutput(OUT_RESULT_PATH,BasicDataTypesTools.stringToStrings(client.OutputResultPath));
